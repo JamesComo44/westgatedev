5765a1cf05f47b73ceb088f72df44c04aee87883
James Como <jamescomo44@gmail.com>
2017-02-01 19:36:36 -0500

    For wg_sermon CPT changed hero image to <h1> and current series heading to <h3>

--- wp-content/themes/westgate-2016/single-wg_sermon.php
+++ wp-content/themes/westgate-2016/single-wg_sermon.php
@@ -1,28 +1,28 @@
 <?php get_header(); ?>
-  
+
   <div id="content">
     <header class="hero">
       <img src="<?php bloginfo('template_directory'); ?>/assets/images/inner_hero.jpg" alt="">
-      <p class="hero-message"><?php echo get_the_title(get_the_id()); ?></p>
+      <h1 class="hero-message"><?php echo get_the_title(get_the_id()); ?></h1>
     </header>
-  
+
     <div id="inner-content" class="row">
-  
+
         <main id="main" role="main" class="medium-10 medium-offset-1">
           <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
             <div class="medium-7 columns sermon-content">
-              
-                  <?php get_template_part( 'parts/loop', 'sermon' ); ?> 
-            </div>        
+
+                  <?php get_template_part( 'parts/loop', 'sermon' ); ?>
+            </div>
             <div class="medium-5 columns sermon-sidebar">
               <aside class="current-series">
                 <header>
-                  <h1 class="current-series-heading">Currently listening to this series:</h1>
+                  <h3 class="current-series-heading">Currently listening to this series:</h3>
                   <?php echo the_post_thumbnail('full') ?>
                 </header>
-                
+
                 <?php
-                  $tax = get_the_terms($post, 'series'); 
+                  $tax = get_the_terms($post, 'series');
                   $slug = $tax[0]->slug;
                   $current_sermon = get_the_id();
 
@@ -44,12 +44,12 @@
                 <ol class="related-sermons-list">
                   <?php while ( $series_query->have_posts() ) :  $series_query->the_post(); ?>
 
-                      <?php 
+                      <?php
                         if ($current_sermon === get_the_id()) {
                            $extra_class=" currently-playing";
                          } else {
                           $extra_class="";
-                         } 
+                         }
                       ?>
 
                       <li class="related-sermons-item<?php echo $extra_class ?>">
@@ -61,10 +61,10 @@
 
                 <a href="/sermons/" class="btn more-series-btn">More Series</a>
               </aside>
-            </div>          
-        <?php endwhile; endif; ?> 
+            </div>
+        <?php endwhile; endif; ?>
       </main> <!-- end #main -->
-        
+
     </div> <!-- end #inner-content -->
 
   </div> <!-- end #content -->
