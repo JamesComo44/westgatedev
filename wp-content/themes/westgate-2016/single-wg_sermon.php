1829300623123df637c58002af1785bc70bed3a3
James Como <jamescomo@cancertutor.com>
2017-05-16 15:59:31 -0400

    Testing changes

--- wp-content/themes/westgate-2016/single-wg_sermon.php
+++ wp-content/themes/westgate-2016/single-wg_sermon.php
@@ -23,7 +23,7 @@
                   $args = array(
                     'post_type' => 'wg_sermon',
                     'order' => 'ASC',
-                    'posts_per_page' => 500,
+                    'posts_per_page' => 100,
                     'tax_query' => array(
                       array(
                         'taxonomy' => 'series',
